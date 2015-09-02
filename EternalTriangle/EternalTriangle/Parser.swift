0ff26a8b5fea690dd5e242ba50efc88092fefac3
ocadaruma <ocadaruma@gmail.com>
2015-09-03 03:05:35 +0900

    implement parsers(WIP)

--- EternalTriangle/EternalTriangle/Parser.swift
+++ EternalTriangle/EternalTriangle/Parser.swift
@@ -8,22 +8,36 @@
 
 import Foundation
 
-//func parseTuneTitle(string: String) -> (TuneTitle?, String) {
-//  if let s = StringScanner(string: string).scan("^T:\\s\\w+$") {
-//    return (nil, string)
-//  } else {
-//    return (nil, string)
-//  }
-//}
+func createParser<T>(pattern: String, op: [MatchResult] -> T) -> String -> (T?, String) {
+  return { (string: String) -> (T?, String) in
+    let scanner = StringScanner(string: string)
+    let matches = scanner.scan(pattern)
+    if matches.isEmpty {
+      return (nil, string)
+    } else {
+      return (op(matches), scanner.result)
+    }
+  }
+}
 
-//func parseComposer(string: String) -> (Composer?, String) {
-//
-//}
-//
-//func parseMeter(string: String) -> (Meter?, String) {
-//
-//}
-//
+public let parseTuneTitle = createParser("^T:\\s*(.+)\n?", { TuneTitle(title: $0[1].match) })
+public let parseComposer = createParser("^C:\\s*(.+)\n?", { Composer(name: $0[1].match) })
+public let parseMeter = createParser("^M:\\s*((?:\\d\\/\\d)|C\\||C)\n?", { (matches: [MatchResult]) -> Meter in
+  var (num, den) = (4, 4)
+  let body = matches[1].match
+  switch body {
+  case "C|":
+    (num, den) = (2, 2)
+  case "C":
+    (num, den) = (4, 4)
+  default:
+    let m = body.matchesWithPattern("(\\d)\\/(\\d)")
+    num = m[1].match.toInt()!
+    den = m[2].match.toInt()!
+  }
+  return Meter(numerator: num, denominator: den)
+})
+
 //func parseUnitNoteLength(string: String) -> (UnitNoteLength?, String) {
 //
 //}
