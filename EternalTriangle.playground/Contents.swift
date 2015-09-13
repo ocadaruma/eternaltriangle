03e1a3eb0853307246bd1bb19aedc4a22a9e5f5e
ocadaruma <ocadaruma@gmail.com>
2015-09-13 16:21:20 +0900

    multiple tracks

--- EternalTriangle.playground/Contents.swift
+++ EternalTriangle.playground/Contents.swift
@@ -4,10 +4,3 @@ import UIKit
 import EternalTriangle
 
 var str = "Hello, playground"
-let s = "__A',,2/3"
-let m = s.matchesWithPattern("(\\^{0,2}|_{0,2}|=?)([a-g]|[A-G])([',]*)(\\d*(?:/\\d+)?)")
-m[3].match
-
-
-"".isEmpty
-
--- EternalTriangle/EternalTriangle/Sequencer.m
+++ EternalTriangle/EternalTriangle/Sequencer.m
@@ -14,19 +14,28 @@
 static void checkError(OSStatus);
 static OSStatus audioUnitCallback(void *,
                                   AudioUnitRenderActionFlags*,
-                                  const AudioTimeStamp *,
+                                  const AudioTimeStamp*,
                                   UInt32,
                                   UInt32,
-                                  AudioBufferList *);
+                                  AudioBufferList*);
+
+static void prepareAUGraph(AUGraph*, AUNode*, AudioUnit*);
+static void loadSF2Preset(UInt8, AudioUnit);
+static MusicTrack createTrack(MusicSequence, AUNode);
+static MusicSequence createSequence(AUGraph);
+static MusicPlayer createPlayer(MusicSequence);
 
 #pragma Class body
 
 @interface Sequencer ()
 
 @property (nonatomic) AUGraph processingGraph;
-@property (nonatomic) AudioUnit samplerUnit;
+@property (nonatomic) AudioUnit mixerUnit;
+@property (nonatomic) AUNode mixerNode;
 @property (nonatomic) MusicPlayer musicPlayer;
 @property (nonatomic) MusicSequence musicSequence;
+
+@property (nonatomic) UInt32 numTracks;
 
 @end
 
@@ -36,12 +45,14 @@ static OSStatus audioUnitCallback(void *,
   self = [super init];
 
   if (self) {
-    [self prepareAUGraph];
-    [self startAUGraph];
-    [self loadSF2Preset:0];
+    self.numTracks = 2;
 
-    self.musicSequence = [self createMusicSequence];
-    self.musicPlayer = [self createPlayer:_musicSequence];
+    prepareAUGraph(&_processingGraph, &_mixerNode, &_mixerUnit);
+    self.musicSequence = createSequence(_processingGraph);
+    self.musicPlayer = createPlayer(_musicSequence);
+
+    [self prepareSamplerNodes];
+    [self startAUGraph];
   }
 
   return self;
@@ -66,42 +77,31 @@ static OSStatus audioUnitCallback(void *,
   checkError(MusicPlayerStop(_musicPlayer));
 }
 
-#pragma private functions
+- (void)prepareSamplerNodes {
+  checkError(AudioUnitSetProperty(_mixerUnit,
+                                  kAudioUnitProperty_ElementCount,
+                                  kAudioUnitScope_Input,
+                                  0,
+                                  &_numTracks, sizeof(_numTracks)));
 
-- (void)prepareAUGraph {
-  // create AUGraph
-  checkError(NewAUGraph(&_processingGraph));
+  for (UInt32 i = 0; i < _numTracks; i++) {
+    // create sampler
+    AUNode samplerNode;
+    AudioComponentDescription desc;
+    desc.componentType = kAudioUnitType_MusicDevice;
+    desc.componentSubType = kAudioUnitSubType_Sampler;
+    desc.componentManufacturer = kAudioUnitManufacturer_Apple;
+    desc.componentFlags = 0;
+    desc.componentFlagsMask = 0;
+    checkError(AUGraphAddNode(_processingGraph, &desc, &samplerNode));
 
-  // create sampler
-  AUNode samplerNode;
-  AudioComponentDescription desc;
-  desc.componentType = kAudioUnitType_MusicDevice;
-  desc.componentSubType = kAudioUnitSubType_Sampler;
-  desc.componentManufacturer = kAudioUnitManufacturer_Apple;
-  desc.componentFlags = 0;
-  desc.componentFlagsMask = 0;
-  checkError(AUGraphAddNode(_processingGraph, &desc, &samplerNode));
+    AudioUnit samplerUnit;
+    checkError(AUGraphNodeInfo(_processingGraph, samplerNode, NULL, &samplerUnit));
+    checkError(AUGraphConnectNodeInput(_processingGraph, samplerNode, 0, _mixerNode, i));
 
-  // create IONode
-  AUNode ioNode;
-  AudioComponentDescription ioUnitDesc;
-  ioUnitDesc.componentType = kAudioUnitType_Output;
-  ioUnitDesc.componentSubType = kAudioUnitSubType_RemoteIO;
-  ioUnitDesc.componentManufacturer = kAudioUnitManufacturer_Apple;
-  ioUnitDesc.componentFlags = 0;
-  ioUnitDesc.componentFlagsMask = 0;
-  checkError(AUGraphAddNode(_processingGraph, &ioUnitDesc, &ioNode));
-
-  // open AUGraph and connect nodes
-  checkError(AUGraphOpen(_processingGraph));
-  checkError(AUGraphNodeInfo(_processingGraph, samplerNode, nil, &_samplerUnit));
-
-  // set callback
-  checkError(AudioUnitAddRenderNotify(_samplerUnit, audioUnitCallback, NULL));
-
-  AudioUnitElement ioUnitOutputElement = 0;
-  AudioUnitElement samplerOutputElement = 0;
-  checkError(AUGraphConnectNodeInput(_processingGraph, samplerNode, samplerOutputElement, ioNode, ioUnitOutputElement));
+    loadSF2Preset(0, samplerUnit);
+    createTrack(_musicSequence, samplerNode);
+  }
 }
 
 - (void)startAUGraph {
@@ -118,66 +118,11 @@ static OSStatus audioUnitCallback(void *,
   }
 }
 
-- (void)loadSF2Preset:(UInt8)preset {
-  NSURL* bankURL = [[NSBundle bundleForClass:self.class] URLForResource:@"vibraphone" withExtension:@"sf2"];
-  AUSamplerInstrumentData instData;
-  instData.fileURL = (__bridge CFURLRef)bankURL;
-  instData.instrumentType = kInstrumentType_DLSPreset;
-  instData.bankMSB = kAUSampler_DefaultMelodicBankMSB;
-  instData.bankLSB = kAUSampler_DefaultBankLSB;
-  instData.presetID = preset;
-
-  checkError(AudioUnitSetProperty(_samplerUnit,
-                                  kAUSamplerProperty_LoadInstrument,
-                                  kAudioUnitScope_Global,
-                                  0,
-                                  &instData,
-                                  sizeof(AUSamplerInstrumentData)));
-}
-
-- (MusicSequence)createMusicSequence {
-  // create the sequence
-  MusicSequence musicSequence;
-  checkError(NewMusicSequence(&musicSequence));
-
-  // add track
-  MusicTrack track;
-  checkError(MusicSequenceNewTrack(musicSequence, &track));
-
-  MusicTimeStamp beat = 1.0;
-  for (UInt8 i = 60; i <= 72; i++) {
-    MIDINoteMessage message;
-    message.channel = 0;
-    message.note = i;
-    message.velocity = 64;
-    message.releaseVelocity = 0;
-    message.duration = 0.2;
-
-    checkError(MusicTrackNewMIDINoteEvent(track, beat, &message));
-    beat += 0.5;
-  }
-
-  checkError(MusicSequenceSetAUGraph(musicSequence, _processingGraph));
-
-  return musicSequence;
-}
-
-- (MusicPlayer)createPlayer:(MusicSequence)sequence {
-  MusicPlayer player;
-  checkError(NewMusicPlayer(&player));
-  checkError(MusicPlayerSetSequence(player, sequence));
-  checkError(MusicPlayerPreroll(player));
-
-  return player;
-}
-
 @end
 
 #pragma C function decls
 static void checkError(OSStatus err) {
-  if (err) {
-    NSLog(@"error occurred. code : %@", @(err));
-  }
+  NSCAssert(err == noErr, @"error code : %@", @(err));
 }
 
 static OSStatus audioUnitCallback(void *inRefCon,
@@ -186,6 +131,97 @@ static OSStatus audioUnitCallback(void *inRefCon,
                                   UInt32 inBusNumber,
                                   UInt32 inNumberFrames,
                                   AudioBufferList *ioData) {
-  NSLog(@"called");
+  NSLog(@"called. inTimeStamp : %@", @(inTimeStamp->mSampleTime));
   return noErr;
+}
+
+static void prepareAUGraph(AUGraph *outGraph, AUNode *mixerNode, AudioUnit *mixerUnit) {
+  // create AUGraph
+  checkError(NewAUGraph(outGraph));
+  checkError(AUGraphOpen(*outGraph));
+
+  // create ioNode
+  AUNode ioNode;
+  AudioUnit ioUnit;
+  AudioComponentDescription ioUnitDesc;
+  ioUnitDesc.componentType = kAudioUnitType_Output;
+  ioUnitDesc.componentSubType = kAudioUnitSubType_RemoteIO;
+  ioUnitDesc.componentManufacturer = kAudioUnitManufacturer_Apple;
+  ioUnitDesc.componentFlags = 0;
+  ioUnitDesc.componentFlagsMask = 0;
+  checkError(AUGraphAddNode(*outGraph, &ioUnitDesc, &ioNode));
+  checkError(AUGraphNodeInfo(*outGraph, ioNode, NULL, &ioUnit));
+
+  // create mixerNode
+  AudioComponentDescription mixerDesc;
+  mixerDesc.componentType = kAudioUnitType_Mixer;
+  mixerDesc.componentSubType = kAudioUnitSubType_MultiChannelMixer;
+  mixerDesc.componentManufacturer = kAudioUnitManufacturer_Apple;
+  mixerDesc.componentFlags = 0;
+  mixerDesc.componentFlagsMask = 0;
+  checkError(AUGraphAddNode(*outGraph, &mixerDesc, mixerNode));
+  checkError(AUGraphNodeInfo(*outGraph, *mixerNode, NULL, mixerUnit));
+
+  // set callback
+  checkError(AudioUnitAddRenderNotify(*mixerUnit, audioUnitCallback, NULL));
+
+  AudioUnitElement ioUnitOutputElement = 0;
+  AudioUnitElement mixerOutputElement = 0;
+  checkError(AUGraphConnectNodeInput(*outGraph, *mixerNode, mixerOutputElement, ioNode, ioUnitOutputElement));
+}
+
+static void loadSF2Preset(UInt8 preset, AudioUnit unit) {
+  NSURL* bankURL = [[NSBundle bundleForClass:[Sequencer class]] URLForResource:@"vibraphone" withExtension:@"sf2"];
+  AUSamplerInstrumentData instData;
+  instData.fileURL = (__bridge CFURLRef)bankURL;
+  instData.instrumentType = kInstrumentType_DLSPreset;
+  instData.bankMSB = kAUSampler_DefaultMelodicBankMSB;
+  instData.bankLSB = kAUSampler_DefaultBankLSB;
+  instData.presetID = preset;
+
+  checkError(AudioUnitSetProperty(unit,
+                                  kAUSamplerProperty_LoadInstrument,
+                                  kAudioUnitScope_Global,
+                                  0,
+                                  &instData,
+                                  sizeof(AUSamplerInstrumentData)));
+}
+
+static MusicTrack createTrack(MusicSequence sequence, AUNode inNode) {
+  MusicTrack track;
+  checkError(MusicSequenceNewTrack(sequence, &track));
+
+  MusicTimeStamp beat = 1.0;
+  for (UInt8 i = 60; i <= 72; i++) {
+    MIDINoteMessage message;
+    message.channel = 0;
+    message.note = i;
+    message.velocity = 64;
+    message.releaseVelocity = 0;
+    message.duration = 0.5;
+
+    checkError(MusicTrackNewMIDINoteEvent(track, beat, &message));
+    beat += 0.5;
+  }
+
+  checkError(MusicTrackSetDestNode(track, inNode));
+
+  return track;
+}
+
+static MusicSequence createSequence(AUGraph inGraph) {
+  MusicSequence musicSequence;
+  checkError(NewMusicSequence(&musicSequence));
+  checkError(MusicSequenceSetAUGraph(musicSequence, inGraph));
+
+  return musicSequence;
+}
+
+static MusicPlayer createPlayer(MusicSequence inSequence) {
+  MusicPlayer player;
+  checkError(NewMusicPlayer(&player));
+  checkError(MusicPlayerSetSequence(player, inSequence));
+  checkError(MusicPlayerPreroll(player));
+
+  return player;
 }
