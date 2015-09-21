db065b9d7412f545ed84022529d7966f1e6d0f2d
ocadaruma <ocadaruma@gmail.com>
2015-09-20 22:40:17 -0700

    renderer skelton

--- EternalTriangle/EternalTriangle/Renderer.swift
+++ EternalTriangle/EternalTriangle/Renderer.swift
@@ -8,8 +8,41 @@
 
 import Foundation
 
-public enum Layout {
-  case Standard
-  case Even
+public class Layout {
+  let numStaff: Int
+  let sheetMargin: UIEdgeInsets
+
+  public init(numStaff: Int, sheetMargin: UIEdgeInsets) {
+    self.numStaff = numStaff
+    self.sheetMargin = sheetMargin
+  }
+
+  public static let defaultLayout: Layout = Layout(numStaff: 4, sheetMargin: UIEdgeInsetsZero)
 }
 
+public class Renderer : UIView {
+  var tune: Tune! = nil
+  var layout: Layout! = nil
+
+  public func render(tune : Tune, layout: Layout = Layout.defaultLayout) {
+    self.layout = layout
+    self.tune = tune
+    setNeedsDisplay()
+  }
+
+  override public func drawRect(rect: CGRect) {
+    renderStaff()
+    renderHeader()
+    renderVoices()
+  }
+
+  private func renderStaff() {
+    let ctx = UIGraphicsGetCurrentContext()
+  }
+
+  private func renderHeader() {
+  }
+
+  private func renderVoices() {
+  }
+}
